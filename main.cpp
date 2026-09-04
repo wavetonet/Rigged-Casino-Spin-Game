14f919e1654e339dc126ff3b9a27abaf0c46866d
Johana Antonette P. Cordova <cordovajohana.edu@gmail.com>
2026-09-04 16:19:11 +0800

    Implement lose mode for slot machine
    Added a lose mode that guarantees a loss on spins.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,67 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
+ 
 using namespace std;
-
+ 
 int main() {
     srand(time(0));
     int coins = 100;
     int command;
     bool isRigged = false;
-
+    bool isLose = false;
     cout << "Slot Machine\n";
     cout << "Player starts with 100 coins\n";
     cout << "Commands: Press 1 for Spin, 2 for Quit, 3 to Toggle Rigged Mode\n";
-
+ 
     while (true) {
         cout << "\nCoins: " << coins << "\n";
         cin >> command;
-
+ 
+ 
+        if (command == 67) {
+            isLose = !isLose;
+            cout << (isLose ? "WARNING THIS MODE WILL MAKE YOU LOSE EVERYTIME\n" : "Lose mode off\n");
+            continue;
+        }
          if (command == 3) {
             isRigged = !isRigged;
             cout << (isRigged ? "RIGGED MODE ON\n" : "RIGGED MODE OFF\n");
             continue;
         }
-
+ 
         if (command == 2) {
             cout << "You chose to quit. Thanks for Playing!\n";
             break;
         }
-
+ 
         if (command == 1) {
             cout << "You chose spin!\n";
             if (coins < 10) {
-                cout << "You don't have enough coins :(\n";
+                cout << "You don't have enough coins :( sorri\n";
                 continue;
             }
-
+ 
             coins -= 10;
             int x, y, z;
-
+ 
             if (isRigged) {
                 x = y = z = 7;
+            } else if (isLose) {
+                x = 6;
+                y = 7;
+                z = 8;
             } else {
-                x = rand() % 8; 
+                x = rand() % 8;
                 y = rand() % 8;
                 z = rand() % 8;
             }
-
+ 
             cout << "[" << x << "]";
             cout << "[" << y << "]";
             cout << "[" << z << "]\n";
-
-            if (x == 7 && y == 7 && z == 7) {
+ 
+            if (x == y && y == z) {
                 cout << "JACKPOTTT! You won 300 coins!\n";
                 coins += 300;
             } else if (x == y && y == z) {
@@ -65,7 +75,6 @@ int main() {
             }
         }
     }
-
+ 
     return 0;
 }
-
